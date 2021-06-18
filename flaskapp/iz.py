485d96ef3327136b6967eb959777cf7e966eb3d1
vitalya5607 <85966705+vitalya5607@users.noreply.github.com>
2021-06-18 23:58:34 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -1,3 +1,4 @@
+print("Hello world")
 from flask import Flask
 app = Flask(__name__)
 #декоратор для вывода страницы по умолчанию
@@ -17,7 +18,7 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 # используем csrf токен, можете генерировать его сами
 SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
-# используем капчу и полученные секретные ключи с сайта google
+# используем капчу и полученные секретные ключи с сайта google 
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lezdz8bAAAAAAxzFfaJYmk-g7X1_nuN_U-_TS3i'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lezdz8bAAAAALNfgbR5rW_dd0lhrS7FokJziFhY'
@@ -31,9 +32,7 @@ class NetForm(FlaskForm):
  # валидатор проверяет введение данных после нажатия кнопки submit
  # и указывает пользователю ввести данные если они не введены
  # или неверны
- cho = StringField('Повернуть на', validators = [DataRequired()])
- gor = StringField('горизонталь', validators = [DataRequired()])
- ver = StringField('вертикаль', validators = [DataRequired()])
+ cho = StringField('1-изменить по вертикали,2-по горизонтали', validators = [DataRequired()])
  # поле загрузки файла
  # здесь валидатор укажет ввести правильные файлы
  upload = FileField('Load image', validators=[
@@ -43,7 +42,6 @@ class NetForm(FlaskForm):
  recaptcha = RecaptchaField()
  #кнопка submit, для пользователя отображена как send
  submit = SubmitField('send')
-
 # функция обработки запросов на адрес 127.0.0.1:5000/net
 # модуль проверки и преобразование имени файла
 # для устранения в имени символов типа / и т.д.
@@ -55,12 +53,14 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-## функция для оброботки изображения
-def draw(filename,cho,gor,ver):
- ##открываем изображение
+## функция для оброботки изображения 
+def draw(filename,cho):
+ ##открываем изображение 
  print(filename)
  img= Image.open(filename)
-
+ x, y = img.size
+ cho=int(cho)
+ 
 ##делаем график
  fig = plt.figure(figsize=(6, 4))
  ax = fig.add_subplot()
@@ -70,20 +70,28 @@ def draw(filename,cho,gor,ver):
  fig.colorbar(b, ax=ax)
  gr_path = "./static/newgr.png"
  sns.displot(data)
+ #plt.show()
  plt.savefig(gr_path)
  plt.close()
 
-##переворот
- img=img.rotate(cho,expand=True)
- output_filename = filename
- img.save(output_filename)
-
-##изменение размера по осям
- size=(gor,ver)
- img = img.resize(size)
- img.save(output_filename)
-
- return output_filename,gr_path,gr_path
+##меняем половинки
+ if cho==1: 
+  a = img.crop((0, 0, int(y * 0.5), x))
+  b = img.crop((int(y * 0.5), 0, x, y))
+  img.paste(b, (0, 0))
+  img.paste(a, (int(x * 0.5), 0))
+  output_filename = filename
+  img.save(output_filename)
+ else:
+  img=img.rotate(90)
+  a = img.crop((0, 0, int(y * 0.5), x))
+  b = img.crop((int(y * 0.5), 0, x, y))
+  img.paste(b, (0, 0))
+  img.paste(a, (int(y * 0.5), 0))
+  img=img.rotate(270)
+  output_filename = filename
+  img.save(output_filename)
+ return output_filename,gr_path
 
 # метод обработки запроса GET и POST от клиента
 @app.route("/net",methods=['GET', 'POST'])
@@ -94,21 +102,19 @@ def net():
  filename=None
  newfilename=None
  grname=None
- grname2=None
  # проверяем нажатие сабмит и валидацию введенных данных
  if form.validate_on_submit():
   # файлы с изображениями читаются из каталога static
   filename = os.path.join('./static', secure_filename(form.upload.data.filename))
   ch=form.cho.data
-  ch=int(ch)
-  osii=form.gor.data
-  osii=int(osii)
-  ra=form.ver.data
-  ra=int(ra)
+ 
   form.upload.data.save(filename)
-  newfilename,grname,grname2 = draw(filename,ch,osii,ra)
+  newfilename,grname = draw(filename,ch)
+ # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
+ # сети если был нажат сабмит, либо передадим falsy значения
+ 
+ return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
 
- return render_template('net.html',form=form,image_name=newfilename,gr_name=grname,gr_name2=grname2)
 
- if __name__ == "__main__":
-  app.run(host='127.0.0.1',port=5000)
+if __name__ == "__main__":
+ app.run(host='127.0.0.1',port=5000)
